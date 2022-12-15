bcc4eaa439459f541497dce1a621a102ec7403e5
Mehdi Dhaouadi <medy.thawedy@googlemail.com>
2022-12-15 18:08:04 +0100

    mediaqueries+main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,38 @@
+console.log(document.getElementById('divImgIcon').style.display);
+document.getElementsByClassName("clsNavItem")[0].style.display;
+
 function displayMenu() {
-    console.log("It works!");
+    console.log("It works!");console.log(document.getElementById('divImgIcon').style.display);
     document.getElementById("divImgIcon").style.display="none";
     document.getElementsByClassName("clsNavItem")[0].style.display = "block";   
     document.getElementsByClassName("clsNavItem")[1].style.display = "block"; 
     document.getElementsByClassName("clsNavItem")[2].style.display = "block"; 
     document.getElementsByClassName("clsNavItem")[3].style.display = "block"; 
+    
 }
 
 function hideIconMenu() {
     console.log("It works!");
+    console.log(document.getElementById('divImgIcon').style.display);
+    console.log(document.getElementsByClassName("clsNavItem")[0].style.display );
+    if(
+    (document.getElementById('divImgIcon').style.display=='none')
+    &&
+    (
+    (document.getElementsByClassName("clsNavItem")[0].style.display == "block")
+    ||
+    (document.getElementsByClassName("clsNavItem")[1].style.display == "block")
+    ||
+    (document.getElementsByClassName("clsNavItem")[2].style.display == "block")
+    ||
+    (document.getElementsByClassName("clsNavItem")[3].style.display == "block")
+    )
+    )
+    {
     document.getElementById("divImgIcon").style.display="flex";
     document.getElementsByClassName("clsNavItem")[0].style.display = "none";   
     document.getElementsByClassName("clsNavItem")[1].style.display = "none"; 
     document.getElementsByClassName("clsNavItem")[2].style.display = "none"; 
     document.getElementsByClassName("clsNavItem")[3].style.display = "none"; 
+    }
 }
